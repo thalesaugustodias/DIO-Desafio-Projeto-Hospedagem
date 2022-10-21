0e63409f072df8c671e5c8d3f5f6c99a70f55749
thales.cooperx <thales.cooperx@gmail.com>
2022-10-20 22:24:10 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Models/Reserva.cs
@@ -0,0 +1,52 @@
+﻿namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= hospedes.Count())
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception(message: "O número de hóspedes excede a capacidade da suíte escolhida.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count();
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+           
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                decimal desconto = valor * 10 / 100;
+                valor = valor - desconto;
+            }
+
+            return valor;
+        }
+    }
+}
